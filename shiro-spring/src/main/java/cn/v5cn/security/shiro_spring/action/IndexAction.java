0cc18a1e3cd2eec5d408ab23b6d3e7fb16e23648
zyw <zyw090111@163.com>
2014-09-16 23:48:49 +0800

    添加CurentUser注解和V5UserFilter

--- shiro-spring/src/main/java/cn/v5cn/security/shiro_spring/action/IndexAction.java
+++ shiro-spring/src/main/java/cn/v5cn/security/shiro_spring/action/IndexAction.java
@@ -3,11 +3,15 @@ package cn.v5cn.security.shiro_spring.action;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.v5cn.security.shiro_spring.annotation.CurrentUser;
+import cn.v5cn.security.shiro_spring.entity.User;
+
 @Controller
 public class IndexAction {
 	
-	@RequestMapping(value="/index")
-	public String index(){
+	@RequestMapping(value={"/","/index"})
+	public String index(@CurrentUser User user){
+		System.out.println(user.getUsername());
 		return "index";
 	}
 }
